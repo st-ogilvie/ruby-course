1cad05ecd7875705b2da8f1a5d89a6909b7e810d
--global <--global>
2021-10-01 11:34:42 -0400

    test pat add

--- hello.rb
+++ hello.rb
@@ -1,8 +1,8 @@
-# Lesson 1: Outputting a String
+# Lesson 1 :: Outputting a String
 
 puts "Hey-o, what's yo' name?"
 
-# Lesson 2: Prompting User Input
+# Lesson 2 :: Prompting User Input
 
 name = gets.chop
 puts "Ah, #{name}! Delighted to make your acquaintance..."
